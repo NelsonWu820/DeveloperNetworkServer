3314ca064738d553f98550e41939a04e12e1c095
Nelson Wu <Nelsonwu820@gmail.com>
2024-02-20 18:17:25 -0500

    changing to get rid of /api

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ connectDB();
 app.use(express.json());
 
 //Define routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
+app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
+app.use("/profile", require("./routes/api/profile"));
+app.use("/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
